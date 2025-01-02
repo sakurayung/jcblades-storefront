a2e2df2cd02f4b0e1cc6f5999383936e2af65375
0Toki <j.ledesma.538374@umindanao.edu.ph>
2025-01-02 18:18:29 +0800

    coockies add async and await only

--- src/lib/data/cookies.ts
+++ src/lib/data/cookies.ts
@@ -1,53 +1,18 @@
 import "server-only"
-import { cookies as nextCookies } from "next/headers"
+import { cookies } from "next/headers"
 
-export const getAuthHeaders = async (): Promise<
-  { authorization: string } | {}
-> => {
-  const cookies = await nextCookies()
-  const token = cookies.get("_medusa_jwt")?.value
+export const getAuthHeaders = async (): Promise<{ authorization: string}  | {}> => {
+  const token = (await cookies()).get("_medusa_jwt")?.value
 
-  if (!token) {
-    return {}
+  if (token) {
+    return { authorization: `Bearer ${token}` }
   }
 
-  return { authorization: `Bearer ${token}` }
-}
-
-export const getCacheTag = async (tag: string): Promise<string> => {
-  try {
-    const cookies = await nextCookies()
-    const cacheId = cookies.get("_medusa_cache_id")?.value
-
-    if (!cacheId) {
-      return ""
-    }
-
-    return `${tag}-${cacheId}`
-  } catch (error) {
-    return ""
-  }
-}
-
-export const getCacheOptions = async (
-  tag: string
-): Promise<{ tags: string[] } | {}> => {
-  if (typeof window !== "undefined") {
-    return {}
-  }
-
-  const cacheTag = await getCacheTag(tag)
-
-  if (!cacheTag) {
-    return {}
-  }
-
-  return { tags: [`${cacheTag}`] }
+  return {}
 }
 
 export const setAuthToken = async (token: string) => {
-  const cookies = await nextCookies()
-  cookies.set("_medusa_jwt", token, {
+  (await cookies()).set("_medusa_jwt", token, {
     maxAge: 60 * 60 * 24 * 7,
     httpOnly: true,
     sameSite: "strict",
@@ -56,20 +21,17 @@ export const setAuthToken = async (token: string) => {
 }
 
 export const removeAuthToken = async () => {
-  const cookies = await nextCookies()
-  cookies.set("_medusa_jwt", "", {
+  (await cookies()).set("_medusa_jwt", "", {
     maxAge: -1,
   })
 }
 
 export const getCartId = async () => {
-  const cookies = await nextCookies()
-  return cookies.get("_medusa_cart_id")?.value
+  return (await cookies()).get("_medusa_cart_id")?.value
 }
 
 export const setCartId = async (cartId: string) => {
-  const cookies = await nextCookies()
-  cookies.set("_medusa_cart_id", cartId, {
+  (await cookies()).set("_medusa_cart_id", cartId, {
     maxAge: 60 * 60 * 24 * 7,
     httpOnly: true,
     sameSite: "strict",
@@ -78,8 +40,5 @@ export const setCartId = async (cartId: string) => {
 }
 
 export const removeCartId = async () => {
-  const cookies = await nextCookies()
-  cookies.set("_medusa_cart_id", "", {
-    maxAge: -1,
-  })
+  (await cookies()).set("_medusa_cart_id", "", { maxAge: -1 })
 }

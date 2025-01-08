4c365e6b5d5b83e131cd8e7ddf65c340d18457fd
sayurinsimp <hello.kenji.dev@gmail.com>
2025-01-08 17:50:22 +0800

    feat: started building about page

--- src/app/[countryCode]/(main)/about/page.tsx
+++ src/app/[countryCode]/(main)/about/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next"
-
 export const metadata: Metadata = {
   title: "About",
   description: "About JC Blades",
@@ -9,10 +8,45 @@ const AboutPage = () => {
   return (
     <div className="content-container">
       <div>
-        <h1>About Page</h1>
+        <h1 className="text-5xl py-6">About JC Blades</h1>
+      </div>
+      <div className="flex flex-row gap-x-36">
+        <div>
+          <img src="/youngowner.png" className="" />
+        </div>
+        <div>
+          <div>
+            <h1>Where passion meets precision, shaping beauty with steel.</h1>
+          </div>
+          <div>
+            <p>
+              Welcome to JC Blades, where every blade tells a story. As a
+              lifelong enthusiast, I've always been captivated by the artistry
+              and precision of blade crafting. What started as a hobby has
+              evolved into a passion project, and I’m thrilled to share my
+              creations with you.
+            </p>
+          </div>
+          <div>
+            <p>
+              Each piece is meticulously handcrafted, combining traditional
+              techniques with modern innovation. I pour my heart and soul into
+              every blade, ensuring it's not just a tool, but a work of art.
+              From the sleekest pocket knife to the most formidable sword,
+              you'll find a blade that resonates with your spirit.
+            </p>
+          </div>
+          <div>
+            <p>
+              Join me on this journey of passion and craftsmanship. Explore our
+              collection and discover the perfect blade for your needs.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
 export default AboutPage
+
--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -1,11 +1,8 @@
-
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import CountryDropdown from "./regions"
 import { User, ShoppingBag } from "@medusajs/icons"
 
-
 export default async function Nav() {
- 
-
   return (
     <header className="m-8">
       <nav className="flex flex-row justify-between items-center">
@@ -13,18 +10,18 @@ export default async function Nav() {
           <h1 className="font-bold text-4xl">JC Blades</h1>
         </div>
         <div className="flex flex-row justify-between gap-x-4 text-xl">
-          <a href="#">About</a>
-          <a href="#">Inspiration</a>
-          <a href="#">Store</a>
+          <LocalizedClientLink href="/about">About</LocalizedClientLink>
+          <LocalizedClientLink href="/inspiration">
+            Inspiration
+          </LocalizedClientLink>
+          <LocalizedClientLink href="/store">Store</LocalizedClientLink>
         </div>
         <div className="w-52 h-auto flex flex-col items-center justify-center">
-        <div className="flex flex-row items-center justify-center gap-x-2">
-          <CountryDropdown/>
-          
-          <User/>
-          <ShoppingBag/>
-         
-         
+          <div className="flex flex-row items-center justify-center gap-x-2">
+            <CountryDropdown />
+
+            <User />
+            <ShoppingBag />
           </div>
         </div>
       </nav>

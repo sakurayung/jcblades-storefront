0a1062bc4daa4c4afe81dc5c7e735e3da3ba5fed
sayurinsimp <hello.kenji.dev@gmail.com>
2025-01-09 18:52:45 +0800

    feat: 80% completion of about page, not responsive and 1:1 to figma design

--- src/app/[countryCode]/(main)/about/page.tsx
+++ src/app/[countryCode]/(main)/about/page.tsx
@@ -8,18 +8,20 @@ const AboutPage = () => {
   return (
     <div className="content-container">
       <div>
-        <h1 className="text-5xl py-6">About JC Blades</h1>
+        <h1 className="text-5xl py-6 mt-24">About JC Blades</h1>
       </div>
-      <div className="flex flex-row gap-x-36">
-        <div>
+      <div className="flex flex-row justify-between">
+        <div className="">
           <img src="/youngowner.png" className="" />
         </div>
-        <div>
+        <div className="flex flex-col gap-y-6 w-[600px]">
           <div>
-            <h1>Where passion meets precision, shaping beauty with steel.</h1>
+            <h2 className="text-3xl">
+              Where passion meets precision, shaping beauty with steel.
+            </h2>
           </div>
           <div>
-            <p>
+            <p className="text-justify text-2xl">
               Welcome to JC Blades, where every blade tells a story. As a
               lifelong enthusiast, I've always been captivated by the artistry
               and precision of blade crafting. What started as a hobby has
@@ -28,7 +30,7 @@ const AboutPage = () => {
             </p>
           </div>
           <div>
-            <p>
+            <p className="text-justify text-2xl">
               Each piece is meticulously handcrafted, combining traditional
               techniques with modern innovation. I pour my heart and soul into
               every blade, ensuring it's not just a tool, but a work of art.
@@ -37,7 +39,7 @@ const AboutPage = () => {
             </p>
           </div>
           <div>
-            <p>
+            <p className="text-justify text-2xl">
               Join me on this journey of passion and craftsmanship. Explore our
               collection and discover the perfect blade for your needs.
             </p>
@@ -49,4 +51,3 @@ const AboutPage = () => {
 }
 
 export default AboutPage
-

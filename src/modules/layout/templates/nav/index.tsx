ffa779e257859d8fffce0cbb8cc7adbd49621de1
sakurayung <jorspacturan13@gmail.com>
2025-03-19 20:21:00 +0800

    changes

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -1,34 +1,3 @@
-// import LocalizedClientLink from "@modules/common/components/localized-client-link"
-// import CountryDropdown from "./regions"
-// import { User, ShoppingBag } from "@medusajs/icons"
-
-// export default async function Nav() {
-//   return (
-//     <header className="m-8">
-//       <nav className="flex flex-row justify-between items-center">
-//         <div>
-//           <h1 className="font-bold text-4xl">JC Blades</h1>
-//         </div>
-//         <div className="flex flex-row justify-between gap-x-4 text-xl">
-//           <LocalizedClientLink href="/about">About</LocalizedClientLink>
-//           <LocalizedClientLink href="/inspiration">
-//             Inspiration
-//           </LocalizedClientLink>
-//           <LocalizedClientLink href="/store">Store</LocalizedClientLink>
-//         </div>
-//         <div className="w-52 h-auto flex flex-col items-center justify-center">
-//           <div className="flex flex-row items-center justify-center gap-x-2">
-//             <CountryDropdown />
-
-//             <User />
-//             <ShoppingBag />
-//           </div>
-//         </div>
-//       </nav>
-//     </header>
-//   )
-// }
-
 'use client'
 import { useEffect, useRef, useState } from 'react';
 import { useWindowScroll } from 'react-use';

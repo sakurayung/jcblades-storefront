24ce17e48d90ffadc2b7cdb80e81107e5965c0dc
sayurinsimp <hello.kenji.dev@gmail.com>
2025-04-10 22:37:41 +0800

    refactor: cleaned code, focusing on proper layouting over responsiveness

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -13,220 +13,24 @@ export default async function Footer() {
   const productCategories = await listCategories()
 
   return (
-    <footer className="border-t border-ui-border-base w-full  bg-black">
-      <div className="content-container flex flex-col w-full">
-        <div className="flex flex-col items-center gap-y-6 xsmall:flex-row justify-between py-28">
+    <footer className="border-t border-ui-border-base w-full bg-[#111210]">
+      <div className="content-container flex flex-col w-full py-32">
+        <div className="flex flex-row justify-between w-[658px]">
           <div>
-            <LocalizedClientLink
-              href="/"
-              className="txt-compact-xlarge-plus text-ui-fg-subtle uppercase font-sans text-white"
-            >
+            <LocalizedClientLink href="/" className="">
               <Image
                 src="/jcbladeslogo.png"
                 alt="JC Blades Logo"
-                width={150}
-                height={150}
+                width={91}
+                height={91}
               />
             </LocalizedClientLink>
           </div>
-          <div className="flex uppercase text-white text-5xl w-[541px] font-rubik text-3xl-footer ">
+          <div className="flex uppercase text-[#FBFEFC] text-5xl w-[547px] font-rubik font-bold">
             BLADES ARE JEWELRY FOR REAL MEN.
-          </div>
-          <div className="text-small-regular gap-10 md:gap-x-16 grid grid-cols-2 sm:grid-cols-3">
-            {productCategories && productCategories?.length > 0 && (
-              <div className="flex flex-col gap-y-2">
-                <span className="txt-small-plus txt-ui-fg-base text-white uppercase font-poppins">
-                  Categories
-                </span>
-                <ul
-                  className="grid grid-cols-1 gap-2"
-                  data-testid="footer-categories"
-                >
-                  {productCategories?.slice(0, 6).map((c) => {
-                    if (c.parent_category) {
-                      return
-                    }
-
-                    const children =
-                      c.category_children?.map((child) => ({
-                        name: child.name,
-                        handle: child.handle,
-                        id: child.id,
-                      })) || null
-
-                    return (
-                      <li
-                        className="flex flex-col gap-2 text-ui-fg-subtle txt-small"
-                        key={c.id}
-                      >
-                        <LocalizedClientLink
-                          className={clx(
-                            "hover:text-ui-fg-base",
-                            children && "txt-small-plus"
-                          )}
-                          href={`/categories/${c.handle}`}
-                          data-testid="category-link"
-                        >
-                          {c.name}
-                        </LocalizedClientLink>
-                        {children && (
-                          <ul className="grid grid-cols-1 ml-3 gap-2">
-                            {children &&
-                              children.map((child) => (
-                                <li key={child.id}>
-                                  <LocalizedClientLink
-                                    className="hover:text-ui-fg-base"
-                                    href={`/categories/${child.handle}`}
-                                    data-testid="category-link"
-                                  >
-                                    {child.name}
-                                  </LocalizedClientLink>
-                                </li>
-                              ))}
-                          </ul>
-                        )}
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-            )}
-            {collections && collections.length > 0 && (
-              <div className="flex flex-col gap-y-2">
-                <span className="txt-small-plus txt-ui-fg-base font-poppins text-white uppercase">
-                  Collections
-                </span>
-                <ul
-                  className={clx(
-                    "grid grid-cols-1 gap-2 text-ui-fg-subtle txt-small",
-                    {
-                      "grid-cols-2": (collections?.length || 0) > 3,
-                    }
-                  )}
-                >
-                  {collections?.slice(0, 6).map((c) => (
-                    <li key={c.id}>
-                      <LocalizedClientLink
-                        className="hover:text-ui-fg-base"
-                        href={`/collections/${c.handle}`}
-                      >
-                        {c.title}
-                      </LocalizedClientLink>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-            <div className="flex flex-col gap-y-2">
-              <span className="txt-small-plus txt-ui-fg-base font-poppins text-white">
-                ORDERS
-              </span>
-              <ul className="grid grid-cols-1 gap-y-2 text-ui-fg-subtle txt-small">
-                <li>
-                  <a
-                    href=""
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    Orders and Delivery
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://docs.medusajs.com"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    Returns and Refund
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://github.com/medusajs/nextjs-starter-medusa"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    Payment and Pricing Policy
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <span className="txt-small-plus txt-ui-fg-base font-poppins text-white">
-                ABOUT
-              </span>
-              <ul className="grid grid-cols-1 gap-y-2 text-ui-fg-subtle txt-small">
-                <li>
-                  <a
-                    href=""
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    About Us
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href=""
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    Contact Us
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href=""
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    FAQs
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="flex justify-between w-full">
-          <div className="flex gap-9">
-            <div className="flex flex-col gap-y-2">
-              <ul className="text-white font-rubik">
-                <a href="">Instagram</a>
-              </ul>
-              <ul className="text-white font-rubik">
-                <a href="">Facebook</a>
-              </ul>
-            </div>
-            <div className="flex flex-col gap-y-2">
-              <ul className="text-white font-rubik">
-                <a href="">YouTube</a>
-              </ul>
-              <ul className="text-white font-rubik">
-                <a href="">TikTok</a>
-              </ul>
-            </div>
-          </div>
-          <div className="flex flex-col gap-y-2">
-            <ul className="text-white font-rubik hover:text-ui-fg-base">
-              <a href="">claymore0three@gmail.com</a>
-            </ul>
-            <ul className="text-white font-rubik">
-              <p>Davao City, 8000</p>
-            </ul>
           </div>
         </div>
         <hr className="w-full my-2 bg-slate-800" />
-        <div className="flex w-full mb-16 justify-between text-ui-fg-muted">
-          <Text className="txt-compact-small">
-            © {new Date().getFullYear()} JC Blades. All rights reserved.
-          </Text>
-        </div>
       </div>
     </footer>
   )

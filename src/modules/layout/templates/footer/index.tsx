a4991a2fa030e60df1c459a9ed433fa7d2aab12e
sayurinsimp <hello.kenji.dev@gmail.com>
2025-04-11 13:23:24 +0800

    refactor: continue working on footer, generalized items, will add later links

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -13,9 +13,9 @@ export default async function Footer() {
   const productCategories = await listCategories()
 
   return (
-    <footer className="border-t border-ui-border-base w-full bg-[#111210]">
-      <div className="content-container flex flex-col w-full py-32">
-        <div className="flex flex-row justify-between w-[658px]">
+    <footer className="w-full bg-[#212427]">
+      <div className="content-container flex flex-row w-full py-32 justify-between">
+        <div className="flex flex-row gap-x-8">
           <div>
             <LocalizedClientLink href="/" className="">
               <Image
@@ -30,7 +30,52 @@ export default async function Footer() {
             BLADES ARE JEWELRY FOR REAL MEN.
           </div>
         </div>
-        <hr className="w-full my-2 bg-slate-800" />
+        <div className="grid grid-cols-4 gap-x-8 text-[#FBFEFC]">
+          <ul className="grid grid-col-1">
+            <span className="font-bold font-poppins text-base mb-3">
+              Category
+            </span>
+            <li className="flex flex-col gap-y-2">
+              <a>Item 1</a>
+              <a>Item 1</a>
+              <a>Item 1</a>
+              <a>Item 1</a>
+            </li>
+          </ul>
+          <ul className="grid grid-col-1">
+            <span className="font-bold font-poppins text-base mb-3">
+              Category
+            </span>
+            <li className="flex flex-col gap-y-2">
+              <a>Item 1</a>
+              <a>Item 1</a>
+              <a>Item 1</a>
+              <a>Item 1</a>
+            </li>
+          </ul>
+          <ul className="grid grid-col-1">
+            <span className="font-bold font-poppins text-base mb-3">
+              Category
+            </span>
+            <li className="flex flex-col gap-y-2">
+              <a>Item 1</a>
+              <a>Item 1</a>
+              <a>Item 1</a>
+              <a>Item 1</a>
+            </li>
+          </ul>
+          <ul className="grid grid-col-1">
+            <span className="font-bold font-poppins text-base mb-3">
+              Category
+            </span>
+            <li className="flex flex-col gap-y-2">
+              <a>Item 1</a>
+              <a>Item 1</a>
+              <a>Item 1</a>
+              <a>Item 1</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   )

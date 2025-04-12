1447160c89ea45b4a3ee9d53fd44007cd1b497ef
sayurinsimp <hello.kenji.dev@gmail.com>
2025-04-12 11:23:47 +0800

    refactor: 90% completion on footer, still lacking responsiveness

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -2,6 +2,7 @@ import { listCategories } from "@lib/data/categories"
 import { listCollections } from "@lib/data/collections"
 import { Text, clx } from "@medusajs/ui"
 import Image from "next/image"
+import Link from "next/link"
 
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import MedusaCTA from "@modules/layout/components/medusa-cta"
@@ -14,68 +15,96 @@ export default async function Footer() {
 
   return (
     <footer className="w-full bg-[#212427]">
-      <div className="content-container flex flex-row w-full py-32 justify-between">
-        <div className="flex flex-row gap-x-8">
-          <div>
-            <LocalizedClientLink href="/" className="">
-              <Image
-                src="/jcbladeslogo.png"
-                alt="JC Blades Logo"
-                width={91}
-                height={91}
-              />
-            </LocalizedClientLink>
+      <div className="content-container w-full py-32">
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row gap-x-8">
+            <div>
+              <LocalizedClientLink href="/" className="">
+                <Image
+                  src="/jcbladeslogo.png"
+                  alt="JC Blades Logo"
+                  width={91}
+                  height={91}
+                />
+              </LocalizedClientLink>
+            </div>
+            <div className="flex uppercase text-[#FBFEFC] text-5xl w-[547px] font-rubik font-bold">
+              BLADES ARE JEWELRY FOR REAL MEN.
+            </div>
           </div>
-          <div className="flex uppercase text-[#FBFEFC] text-5xl w-[547px] font-rubik font-bold">
-            BLADES ARE JEWELRY FOR REAL MEN.
-          </div>
+          <div className="grid grid-cols-4 gap-x-8 text-[#FBFEFC]">
+            <ul className="grid grid-col-1">
+              <span className="font-bold font-poppins text-base mb-3">
+                Category
+              </span>
+              <li className="flex flex-col gap-y-2">
+                <a>Item 1</a>
+                <a>Item 1</a>
+                <a>Item 1</a>
+                <a>Item 1</a>
+              </li>
+            </ul>
+            <ul className="grid grid-col-1">
+              <span className="font-bold font-poppins text-base mb-3">
+                Category
+              </span>
+              <li className="flex flex-col gap-y-2">
+                <a>Item 1</a>
+                <a>Item 1</a>
+                <a>Item 1</a>
+                <a>Item 1</a>
+              </li>
+            </ul>
+            <ul className="grid grid-col-1">
+              <span className="font-bold font-poppins text-base mb-3">
+                Category
+              </span>
+              <li className="flex flex-col gap-y-2">
+                <a>Item 1</a>
+                <a>Item 1</a>
+                <a>Item 1</a>
+                <a>Item 1</a>
+              </li>
+            </ul>
+            <ul className="grid grid-col-1">
+              <span className="font-bold font-poppins text-base mb-3">
+                Category
+              </span>
+              <li className="flex flex-col gap-y-2">
+                <a>Item 1</a>
+                <a>Item 1</a>
+                <a>Item 1</a>
+                <a>Item 1</a>
+              </li>
+            </ul>
+          </div></div>
+        <div className="flex flex-row gap-x-8 mb-3">
+          <Link href="https://www.facebook.com/claymore2003">
+            <Image src="/facebook-svgrepo-com.svg"
+              alt="Facebook Logo"
+              width={28}
+              height={28} />
+          </Link>
+          <Link href="https://www.instagram.com/clay2003more/">
+            <Image src="/instagram-svgrepo-com(1).svg"
+              alt="Instagram Logo"
+              width={28}
+              height={28} />
+          </Link>
+          <Link href="https://www.youtube.com/@klamzsharpstuff">
+            <Image src="/youtube-svgrepo-com.svg"
+              alt="Youtube Logo"
+              width={28}
+              height={28} />
+          </Link>
+          <Link href="https://www.tiktok.com">
+            <Image src="/tiktok-svgrepo-com.svg"
+              alt="TikTok Logo"
+              width={28}
+              height={28} />
+          </Link>
         </div>
-        <div className="grid grid-cols-4 gap-x-8 text-[#FBFEFC]">
-          <ul className="grid grid-col-1">
-            <span className="font-bold font-poppins text-base mb-3">
-              Category
-            </span>
-            <li className="flex flex-col gap-y-2">
-              <a>Item 1</a>
-              <a>Item 1</a>
-              <a>Item 1</a>
-              <a>Item 1</a>
-            </li>
-          </ul>
-          <ul className="grid grid-col-1">
-            <span className="font-bold font-poppins text-base mb-3">
-              Category
-            </span>
-            <li className="flex flex-col gap-y-2">
-              <a>Item 1</a>
-              <a>Item 1</a>
-              <a>Item 1</a>
-              <a>Item 1</a>
-            </li>
-          </ul>
-          <ul className="grid grid-col-1">
-            <span className="font-bold font-poppins text-base mb-3">
-              Category
-            </span>
-            <li className="flex flex-col gap-y-2">
-              <a>Item 1</a>
-              <a>Item 1</a>
-              <a>Item 1</a>
-              <a>Item 1</a>
-            </li>
-          </ul>
-          <ul className="grid grid-col-1">
-            <span className="font-bold font-poppins text-base mb-3">
-              Category
-            </span>
-            <li className="flex flex-col gap-y-2">
-              <a>Item 1</a>
-              <a>Item 1</a>
-              <a>Item 1</a>
-              <a>Item 1</a>
-            </li>
-          </ul>
-        </div>
+        <hr className="w-full bg-slate-900 "></hr>
       </div>
     </footer>
   )
